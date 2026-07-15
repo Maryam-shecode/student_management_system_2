6b33c64217ea0e342c9264e5cf943cceb92de88a
Nilo <bigzadmaryam112@gmail.com>
2026-07-15 21:51:23 +0600

    course class

--- course.py
+++ course.py
@@ -1,2 +1,20 @@
 class Course:
-    def __init__(self, ):
+    def __init__(self,course_code, course_name):
+        self.course_code=course_code
+        self.course_name=course_name
+        self.students=[]
+        self.assessments=[]
+    def add_student(self,student_id):
+        self.students.append(student_id)
+    def add_assessment (self, assessment):
+        self.assessments.append(assessment)
+    def find_assessment(self,title):
+        for assessment in self.assessments:
+            if assessment.title==title:
+                return assessment
+        return None
+    def display_info(self):
+        print("Course code: ", self.course_code)
+        print("Course name: ", self.course_name)
+        print("Students: ", self.students)
+        print("Assessments: ")
--- student.py
+++ student.py
@@ -1,15 +1,15 @@
 class Student:
     def __init__(self, name, studend_Id, email):
         self.name=name
-        self.studend_Id=studend_Id
+        self.studend_id=studend_Id
         self.email=email
         self.courses=[]
-    def display_innfo(self):
-        print("Student ID: ", self.studend_Id)
+    def display_info(self):
+        print("Student ID: ", self.studend_id)
         print(" Name: ", self.name)
         print(" Email Address: ", self.email)
     def get_id(self):
-        return self.studend_Id
+        return self.studend_id
     def get_name(self):
         return self.name
     def get_email(self):
