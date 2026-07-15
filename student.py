bb0212df48ffd29708e560752995f45b7b1e39e7
Nilo <bigzadmaryam112@gmail.com>
2026-07-15 21:26:42 +0600

    updated student

--- student.py
+++ student.py
@@ -1,2 +1,12 @@
-class student:
+class Student:
+    def __init__(self, name, studend_Id, email):
+        self.name=name
+        self.studend_Id=studend_Id
+        self.email=email
+    def display_innfo(self):
+        print("Student ID: ", self.studend_Id)
+        print(" Name: ", self.name)
+        print(" Email Address: ", self.email)
+    def get_id(self):
+        return self.studend_Id
     
