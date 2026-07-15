4cbb361249cb96ca14745ca72bec88e5a3805300
Nilo <bigzadmaryam112@gmail.com>
2026-07-15 21:37:32 +0600

    done student class
    I used append because course is a list. this option will add new items in the end of list.
    I used [ ] in course because it can be a list of course

--- student.py
+++ student.py
@@ -3,10 +3,24 @@ class Student:
         self.name=name
         self.studend_Id=studend_Id
         self.email=email
+        self.courses=[]
     def display_innfo(self):
         print("Student ID: ", self.studend_Id)
         print(" Name: ", self.name)
         print(" Email Address: ", self.email)
     def get_id(self):
         return self.studend_Id
-    
+    def get_name(self):
+        return self.name
+    def get_email(self):
+        return self.email
+    def set_email(self, email):
+        if "@" in email:
+            self.email=email
+            print("Email has been recorded successfully")
+        else:
+            print("invalid")
+    def enroll_course(self, course_code):
+        self.courses.append(course_code)
+
+
