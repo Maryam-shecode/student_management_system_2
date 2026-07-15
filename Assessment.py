9c1f785285ca6677a2f2e4cef491d202e0d4c1e2
Nilo <bigzadmaryam112@gmail.com>
2026-07-15 22:07:59 +0600

    assessment

--- Assessment.py
+++ Assessment.py
@@ -1,3 +1,18 @@
 class Assessment:
     def __init__(self, title, max_score):
-        self 
+        self.title=title
+        self.max_score=max_score
+    def calculate_percentage(self,score):
+        percentage=(score/self.max_score)*100
+        return percentage
+    def grade_message(self,score):
+        percentage=self.calculate_percentage(score)
+        if percentage>=90:
+            return "Excellent!"
+        elif percentage>=80:
+            return "Nice"
+        elif percentage>=70:
+            return "Good"
+        else:
+            return" You need to study hard! "
+        
