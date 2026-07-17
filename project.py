b6c1ae29c68d8753665eb733c7ac89a50c0c437e
Nilo <bigzadmaryam112@gmail.com>
2026-07-17 20:48:19 +0600

    done project class

--- project.py
+++ project.py
@@ -1,3 +1,18 @@
 from assessment import Assessment
-class project(Assessment):
-    
+class Project(Assessment):
+    def display_info(self):
+        print("Project: ", self.title)
+        print("Maximum score: ", self.max_score)
+    def grade_message(self,score):
+        percentage =self.calculate_percentage(score)
+        if percentage>=95:
+            return "Excellent Job!"
+        elif percentage>=85:
+            return "well done"
+        elif percentage>=75:
+            return "project submitted"
+        else:
+            return "project needs improvement"
+
+
+
