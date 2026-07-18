220a052d964836d928ca32fcf51581eec3245120
Nilo <bigzadmaryam112@gmail.com>
2026-07-18 23:59:42 +0600

    update grade book

--- graded_book.py
+++ graded_book.py
@@ -8,4 +8,26 @@ class Graded_book:
         self.student[student.student_id]=student
     def add_course(self,course):
         self.courses[course.course_code]= course
-    def add_grade(self,):
+    def enrol_student(self, student_id, course_code):
+        if student_id in self.student:
+            if course_code in self.courses:
+                self.student[student_id].enrol_course(course_code)
+                print("student enrolled successfully.")
+            else:
+                print("course is not found")
+        else:
+            print("student is not found")
+    def add_assessment(self, course_code, assessment):
+        if course_code in self.courses:
+            self.courses[course_code].add_assessement(assessment)
+            print("Assessment is added")
+        else:
+            print("Course is not found!")
+    def record_grade(self,student_id, course_code, assessment_title, score):
+        if student_id not in self.grade:
+            self.grade[student_id]={}
+        student_grade=self.grade[student_id]
+        if course_code not in self.grade[student_id]:
+            self.
+
+
