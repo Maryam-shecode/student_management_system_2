6ede3dac48439a74a2f071617aaa0607c72334be
Nilo <bigzadmaryam112@gmail.com>
2026-07-19 08:54:17 +0600

    main class  update

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from student import Student
+from course import Course
+from assessment import Assessment
+from quiz import Quiz
+from exam import Exam
+from project import Project
+from graded_book import Gradedbook
+
+
