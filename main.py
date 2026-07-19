14c9049e4876d9c35fe59ffa5104da20308fe602
Nilo <bigzadmaryam112@gmail.com>
2026-07-19 09:24:09 +0600

    update student

--- main.py
+++ main.py
@@ -5,5 +5,50 @@ from quiz import Quiz
 from exam import Exam
 from project import Project
 from graded_book import Gradedbook
+graded_book=Gradedbook
+choice=""
+while choice !="0":
+     print("******* Student Grade Book system *******")
+     print("1.Add student: ")
+     print("2. Add course: ")
+     print("3. Add Enroll student: ")
+     print("4. Add Assessment: ")
+     print("5. Record Grade ")
+     print("6. Show Report")
+     print("7. Search student:")
+     print("8. Delete student: ")
+     print("9. Dashboard: ")
+     print("0. Exit")
+
+     choice= input("Enter your a number: ")
+     if choice =="1":
+         name= input("Enter student name: ")
+         Student_id=input("Enter student Id: ")
+         email=input ("Enter student email: ")
+         student=Student(name,student_id, email)
+         graded_book.add_student(student)
+
+     elif choice=="2":
+         course_code=input("Enterr course code: ")
+         course_name=input("Enter course name: ")
+         course=Course(course_code,course_name)
+         graded_book.add_course(course)
+
+     elif choice=="3":
+         student_id= input("Enter student Id: ")
+         course_code=input("Enter course code: ")
+         graded_book.enrol_student(student_id,course_code)
+     elif choice=="4":
+         course_code=input("Enter course code: ")
+         print("1. Quiz!")
+         print("2.Exam")
+         print("3. project")
+         assessment_type=input("Choose assessement type: ")
+         title_type=input("Enter assessment title type: ")
+         max_score=input("Enter maximum score: ")
+         
+
+
+
 
 
--- student.py
+++ student.py
@@ -1,15 +1,15 @@
 class Student:
-    def __init__(self, name, studend_Id, email):
+    def __init__(self, name, student_id, email):
         self.name=name
-        self.studend_id=studend_Id
+        self.student_id=student_id
         self.email=email
         self.courses=[]
     def display_info(self):
-        print("Student ID: ", self.studend_id)
+        print("Student ID: ", self.student_id)
         print(" Name: ", self.name)
         print(" Email Address: ", self.email)
     def get_id(self):
-        return self.studend_id
+        return self.student_id
     def get_name(self):
         return self.name
     def get_email(self):
